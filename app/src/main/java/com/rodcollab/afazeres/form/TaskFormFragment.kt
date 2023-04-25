5463b112f0210bd5cb86ed71e5f37a6806ebceb4
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-04-25 12:30:21 -0300

    improving category field put a ChipGroup instead of EditText and add a switchCompat to enable or disable alarm and add resources

--- app/src/main/java/com/rodcollab/afazeres/form/TaskFormFragment.kt
+++ app/src/main/java/com/rodcollab/afazeres/form/TaskFormFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.rodcollab.afazeres.R
 import com.rodcollab.afazeres.databinding.FragmentTaskFormBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
@@ -43,7 +45,32 @@ class TaskFormFragment : Fragment() {
 
         setupDateField()
 
+        binding.setAlarmSwitch.setOnCheckedChangeListener { _, isChecked ->
+
+            setupIcon(isChecked)
+
+        }
+
         binding.saveButton.setOnClickListener { onSave() }
+    }
+
+    private fun setupIcon(isChecked: Boolean) {
+        val icon = AppCompatResources.getDrawable(requireContext(), setIcon(isChecked))
+        binding.setAlarmSwitch.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            icon,
+            null,
+            null,
+            null
+        )
+    }
+
+    private fun setIcon(isChecked: Boolean): Int {
+        return when (isChecked) {
+            true -> R.drawable.alarm_on
+            else -> {
+                R.drawable.alarm_off
+            }
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -77,7 +104,14 @@ class TaskFormFragment : Fragment() {
 
         val habitName = binding.titleTextInput.editText?.text.toString()
 
-        val habitCategory = binding.categoryTextInput.editText?.text.toString()
+        val habitCategory = when (binding.categoryChipGroup.checkedChipId) {
+            R.id.category_study -> "Study"
+            R.id.category_personal -> "Personal"
+            else -> {
+                "Work"
+            }
+        }
+
 
         val habitDate = binding.dateTextInput.editText?.text.toString()
 
