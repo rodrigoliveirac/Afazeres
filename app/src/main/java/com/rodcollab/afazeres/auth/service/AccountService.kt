c19964b1f0f191ffd6b28a94eaaf6810aebf9ee7
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-05-09 19:00:45 -0300

    add di for firebase

---
+++ app/src/main/java/com/rodcollab/afazeres/auth/service/AccountService.kt
@@ -0,0 +1,15 @@
+package com.rodcollab.afazeres.auth.service
+
+import kotlinx.coroutines.flow.Flow
+
+interface AccountService {
+
+    val currentUserId: String
+    val hasUser: Boolean
+
+    val currentUser: Flow<User>
+
+    suspend fun authenticate(email:String, password:String)
+
+    suspend fun createAccount(email:String, password:String, isSuccessful: (Boolean) -> Unit)
+}
